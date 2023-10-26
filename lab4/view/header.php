8d3ee670ca400b37433cad1b81fd0567ddc16cbd
Sulgard <artur.sutula90@gmail.com>
2023-10-26 20:35:00 +0200

    little changes to header

--- lab4/view/header.php
+++ lab4/view/header.php
@@ -1,9 +1,17 @@
 <header>
             <nav class="navbar">
-                <ul>
+                <ul class="navbar">
                     <li><a href="index.php">Menu</a></li>
                     <li><a href="klasy.php">Klasy</a></li>
-                    <li><a href="fabuła.php">Fabuła</a></li>
+                    <li>Fabuła
+                        <ul>
+                            <li><a href="akt1.php">Akt I</a></li>
+                            <li><a href="#">Akt II</a></li>
+                            <li><a href="#">Akt III</a></li>
+                            <li><a href="#">Akt VI</a></li>
+                            <li><a href="#">Akt V</a></li>
+                        </ul>
+                    </li>
                     <li><a href="akt1.php">Akt I</a></li>
                     <li><a href="kontakt.php">Kontakt</a></li>
                     <li><a href="html/jstest.html">testJs</a></li>
