8cee6235220788808eea1451c79bd3aecc76b573
Sulgard <73705012+Sulgard@users.noreply.github.com>
2023-12-20 14:32:19 +0100

    I forgot to pull the changes properly

---
+++ lab9/view/header.php
@@ -0,0 +1,42 @@
+<header>
+    <ul>
+         <!-- Menu główne -->
+        <li><a href="index.php">Menu</a></li>
+        <!-- Sekcja "Klasy" z rozwijanym podmenu -->
+        <li>
+            <a href="#">Klasy</a>
+            <ul class="dropdown">
+                <li><a href="Amazonka.php">Amazonka</a></li>
+                <li><a href="Zabójczyni.php">Zabójczyni</a></li>
+                <li><a href="Barbarzyńca.php">Barbarzyńca</a></li>
+                <li><a href="Druid.php">Druid</a></li>
+                <li><a href="Paladyn.php">Paladyn</a></li>
+                <li><a href="Czarodziejka.php">Czarodziejka</a></li>
+                <li><a href="Nekromanta.php">Nekromanta</a></li>
+            </ul>
+        </li>
+         <!-- Sekcja "Fabuła" z rozwijanym podmenu -->
+        <li>
+            <a href="#">Fabuła</a>
+            <ul class="dropdown">
+                <li><a href="akt1.php">Akt I</a></li>
+                <li><a href="akt2.php">Akt II</a></li>
+                <li><a href="akt3.php">Akt III</a></li>
+                <li><a href="akt4.php">Akt VI</a></li>
+                <li><a href="akt5.php">Akt V</a></li>
+            </ul>
+        </li>
+        <!-- Sekcja "Laby" z rozwijanym podmenu -->
+        <li>
+            <a href="#">Laby</a>
+            <ul class="dropdown">
+                <li><a href="html/jstest.html">Lab3js1</a></li>
+                <li><a href="html/jstest2.html">Lab3js2</a></li>
+                <li><a href="labor_4_162598_nr_4ISI.php">Lab4</a></li>
+            </ul>
+        </li>
+        <!-- Pozostałe odnośniki w menu -->
+        <li><a href="kontakt.php">Kontakt</a></li>
+        <li><a href="./admin/admin.php">Admin</a></li>
+        </ul>
+</header>
